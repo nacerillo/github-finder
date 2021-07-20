f92d742b39f8513c3351747178fb63cdeb3405fe
nacerillo <nicholascerillo@gmail.com>
2021-07-20 14:38:52 -0400

    adding notfound page, and moving search and user components into a new hom component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useState, Fragment} from "react";
+import React from "react";
 import './App.css';
-import Search from "./components/users/Search";
-import Users from "./components/users/Users";
+import  Home  from "./components/pages/Home";
 import Alert from "./components/layout/Alert";
 import NavBar from "./components/layout/NavBar";
+import NotFound from "./components/pages/NotFound";
 import About from "./components/pages/About";
 import User from "./components/users/User";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
@@ -84,15 +84,10 @@ const App = () => {
         <div className = "container">
           <Alert/>
           <Switch>
-            <Route exact path= "/" render={props => (
-              <Fragment>
-                 <Search />
-                <Users />
-              </Fragment>
-            )}/>
+            <Route exact path= "/" component={Home}/>
             <Route exact path = "/about" component = {About}/>
-            <Route exact path = "/user/:login" component = {User}
-            />
+            <Route exact path = "/user/:login" component = {User}/>
+            <Route component = {NotFound}/>
           </Switch>
  
         </div>
---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+ const NotFound = () => {
+    return (
+        <div>
+            <h1>
+                No one here but us chickens!
+            </h1>
+            <p className = "lead">the page you are looking for does not exist</p>
+        </div>
+    )
+}
+export default NotFound;
